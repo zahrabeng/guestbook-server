02b921df96decdadd9c7e36b5551e39435dc9b5a
Richard Ng <richardcrng@gmail.com>
2021-11-15 20:21:08 +0000

    Add documentation to database helpers

--- src/db.ts
+++ src/db.ts
@@ -11,8 +11,15 @@ export interface GuestbookSignatureWithId extends GuestbookSignature {
 
 const db: GuestbookSignatureWithId[] = [];
 
+/** Variable to keep incrementing id of guestbook signatures */
 let idCounter = 0;
 
+/**
+ * Adds in some dummy guestbook signatures to the database
+ * 
+ * @param n - the number of signatures to generate
+ * @returns the created signatures
+ */
 export const addDummyGuestbookSignatures = (n: number): GuestbookSignatureWithId[] => {
   const createdSignatures: GuestbookSignatureWithId[] = [];
   for (let count = 0; count < n; count++) {
@@ -25,6 +32,12 @@ export const addDummyGuestbookSignatures = (n: number): GuestbookSignatureWithId
   return createdSignatures;
 }
 
+/**
+ * Adds in a single signature to the database
+ * 
+ * @param data - the signature data to insert in
+ * @returns the signature added (with a newly created id)
+ */
 export const addGuestbookSignature = (data: GuestbookSignature): GuestbookSignatureWithId => {
   const newEntry: GuestbookSignatureWithId = {
     id: ++idCounter,
@@ -34,14 +47,31 @@ export const addGuestbookSignature = (data: GuestbookSignature): GuestbookSignat
   return newEntry;
 };
 
-export const deleteGuestbookSignatureById = (id: number): void => {
+/**
+ * Deletes a guestbook signature with the given id
+ * 
+ * @param id - the id of the guestbook signature to delete
+ * @returns the deleted guestbook signature (if originally located),
+ *  otherwise the string `"not found"`
+ */
+export const deleteGuestbookSignatureById = (id: number): GuestbookSignatureWithId | "not found" => {
   const idxToDeleteAt = findIndexOfGuestbookSignatureById(id);
-  // type guard against undefined
   if (typeof idxToDeleteAt === 'number') {
-    db.splice(idxToDeleteAt, 1);
+    const signatureToDelete = getGuestbookSignatureById(id);
+    db.splice(idxToDeleteAt, 1); // .splice can delete from an array
+    return signatureToDelete;
+  } else {
+    return "not found"
   };
 }
 
+/**
+ * Finds the index of a guestbook signature with a given id
+ * 
+ * @param id - the id of the guestbook signature to locate the index of
+ * @returns the index of the matching guestbook signature,
+ *  otherwise the string `"not found"`
+ */
 const findIndexOfGuestbookSignatureById = (id: number): number | "not found" => {
   const matchingIdx = db.findIndex((entry) => entry.id === id);
   // .findIndex returns -1 if not located
@@ -52,10 +82,21 @@ const findIndexOfGuestbookSignatureById = (id: number): number | "not found" => 
   }
 };
 
+/**
+ * Find all guestbook signatures
+ * @returns all guestbook signatures from the database
+ */
 export const getAllGuestbookSignatures = (): GuestbookSignatureWithId[] => {
   return db;
 }
 
+/**
+ * Locates a guestbook signature by a given id
+ * 
+ * @param id - the id of the guestbook signature to locate
+ * @returns the located guestbook signature (if found),
+ *  otherwise the string `"not found"`
+ */
 export const getGuestbookSignatureById = (id: number): GuestbookSignatureWithId | "not found" => {
   const maybeEntry = db.find(entry => entry.id === id);
   if (maybeEntry) {
@@ -65,6 +106,15 @@ export const getGuestbookSignatureById = (id: number): GuestbookSignatureWithId 
   };
 }
 
+/**
+ * Applies a partial update to a guestbook signature for a given id
+ *  based on the passed data
+ * 
+ * @param id - the id of the guestbook signature to update
+ * @param newData - the new data to overwrite
+ * @returns the updated guestbook signature (if one is located),
+ *  otherwise the string `"not found"`
+ */
 export const updateGuestbookSignatureById = (id: number, newData: Partial<GuestbookSignature>): GuestbookSignatureWithId | "not found" => {
   const idxOfEntry = findIndexOfGuestbookSignatureById(id);
   // type guard against "not found"
